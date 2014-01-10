704e3859da4f06a1ff8da4006cfb209c7c68801c
Sam Kottler <shk@redhat.com>
2014-01-10 14:30:43 +0100

    Add a simple autoloader for features and a naming convention

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -4,6 +4,10 @@ module Proxy
   MODULES = %w{dns}
   VERSION = "1.3-poc"
 
+  # This is really ugly and needs refactoring, but it works fine for now and
+  # global feature def is okay since it won't change after initial load.
+  $features = []
+
   require "settings"
   ::SETTINGS = Settings.load_from_file
 
@@ -12,11 +16,29 @@ module Proxy
   require "pathname"
   require "log"
   require "util"
-  require "dns/dns"         if SETTINGS.dns
   require "either"
+  require "active_support/all"
+
+
+  Dir.foreach("#{File.dirname(__FILE__)}") do |feature|
+    # Dir#foreach includes the current and parent dir, so just skip them.
+    next if feature == '.' or feature == '..'
+    if File.exist?("#{File.dirname(__FILE__)}/#{feature}/#{feature}.rb")
+      begin
+        require "#{File.dirname(__FILE__)}/#{feature}/#{feature}.rb"
+        $features << feature
+      rescue LoadError
+        # Just don't load the feature if it doesn't have a proper definition,
+        # although this should ultimately be some kind of warning.
+      end
+    end
+  end
 
   class ProxySupervisor < ::Celluloid::SupervisionGroup
-    supervise SmartProxy::DnsActor, as: :dns if SETTINGS.dns
+    $features.each do |feature|
+      klass = "SmartProxy::#{feature.capitalize}Actor".constantize
+      supervise klass, as: feature.to_sym if SETTINGS.feature
+    end
   end
   ProxySupervisor.run!
 
